2bcab6c5658816bc115f0f2191d147203791ab0d
arcadij <arcadij@techs.com.ua>
2014-04-25 17:11:47 +0300

    qf

--- protected/config/main.php
+++ protected/config/main.php
@@ -75,35 +75,35 @@ $config = array(
 					'js' => array(YII_DEBUG ? 'jquery-1.11.0.js' : 'jquery-1.11.0.min.js'),
 				),
                 'chosen' => array(
-                    'baseUrl' => '/theme/ace/assets/',
+                    'baseUrl' => 'theme/ace/assets/',
                     'css' => array('css/chosen.css'),
                     'js' => array('js/chosen.jquery.min.js'),
                     'depends' => array('jquery')
                 ),
                 'gritter' => array(
-                    'baseUrl' => '/theme/ace/assets/',
+                    'baseUrl' => 'theme/ace/assets/',
                     'css' => array('css/jquery.gritter.css'),
                     'js' => array('js/jquery.gritter.min.js'),
                     'depends' => array('jquery')
                 ),
                 'spin' => array(
-                    'baseUrl' => '/theme/ace/assets/',
+                    'baseUrl' => 'theme/ace/assets/',
                     'js' => array('js/spin.min.js'),
                     'depends' => array('jquery')
                 ),
                 'jqgrid' => array(
-                    'baseUrl' => '/theme/ace/assets/',
+                    'baseUrl' => 'theme/ace/assets/',
                     'css' => array('css/ui.jqgrid.css'),
                     'js' => array('js/jqGrid/jquery.jqGrid.min.js'),
                     'depends' => array('jquery')
                 ),
                 'dataTables' => array(
-                    'baseUrl' => '/theme/ace/assets/',
+                    'baseUrl' => 'theme/ace/assets/',
                     'js' => array('js/jquery.dataTables.min.js', 'js/jquery.dataTables.bootstrap.js'),
                     'depends' => array('jquery')
                 ),
                 'daterangepicker' => array(
-                    'baseUrl' => '/theme/ace/assets/',
+                    'baseUrl' => 'theme/ace/assets/',
                     'css' => array('css/daterangepicker.css'),
                     'js' => array('js/date-time/daterangepicker.min.js', 'js/date-time/moment.min.js'),
                     'depends' => array('jquery')
