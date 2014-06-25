9f71cfcc98a721b63887295498b77ca8d4bf85ba
arcadij <arcadij@techs.com.ua>
2014-06-25 14:56:01 +0300

    qf

--- protected/views/entrance/rating/_bottom.php
+++ protected/views/entrance/rating/_bottom.php
@@ -178,7 +178,7 @@ HTML;
                     $html .= <<<HTML
                         <tr><td colspan='8' style='text-decoration:underline;'>{$title}</td></tr>
 HTML;
-
+                    $html .= $inContest;
                 }
                 echo $html;
             endif;
