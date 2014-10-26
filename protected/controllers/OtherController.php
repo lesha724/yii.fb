a04d79ce80307e97a9f35ec2526843e382d1245a
arkadij <arkadij>
2014-10-26 12:11:16 +0200

    qf

--- protected/controllers/OtherController.php
+++ protected/controllers/OtherController.php
@@ -206,7 +206,8 @@ class OtherController extends Controller
             if ($isEditable && isset($_REQUEST['Sdp'])) {
 
                 $model->attributes = $_REQUEST['Sdp'];
-                if ($model->validate()) {
+                $attr = array('sdp4', 'sdp26', 'sdp27', 'sdp28', 'sdp30', 'sdp31', 'sdp32', 'sdp33');
+                if ($model->validate($attr)) {
                     $model->save();
                 } else
                     Yii::app()->user->setFlash('error', tt('Пожалуйста, исправьте возникшие ошибки!'));
