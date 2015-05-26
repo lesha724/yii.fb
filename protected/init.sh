156fc473ba0b8bf3e9e8e5021e8429c67824afdf
root <root@rating>
2015-05-26 12:42:39 +0300

    modified:   protected/init.sh

--- protected/init.sh
+++ protected/init.sh
@@ -2,6 +2,7 @@
 chmod 777 ./assets
 chmod 777 ./protected/runtime
 chmod 777 ./protected/runtime/cache
+chmod 777 ./protected/config/params.inc
 chmod 755 ./protected/yiic
 
 # Create local config file if not exits
