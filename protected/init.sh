d8aa15278bc4837c440fa5f19e17584600ce0424
lesha724 <n3ff_alewka@mail.ru>
2015-08-30 14:40:22 +0300

    paramc.inc
    in init.sh and ignore

--- protected/init.sh
+++ protected/init.sh
@@ -9,5 +9,7 @@ chmod 755 ./protected/yiic
 # Create local config file if not exits
 test -f ./protected/config/main-local.php || cp ./protected/config/main-local.example.php ./protected/config/main-local.php
 
+touch ./protected/config/params.inc
+chmod 777 ./protected/config/params.inc
 touch ./protected/data/menu.txt
 chmod 777 ./protected/data/menu.txt
