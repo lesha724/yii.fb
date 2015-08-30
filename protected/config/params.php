e55fc7f40a795321b61cc9ffe655b083abd0485a
lesha724 <n3ff_alewka@mail.ru>
2015-08-30 14:45:09 +0300

    params.php
    fileexsits and !empty

--- protected/config/params.php
+++ protected/config/params.php
@@ -6,8 +6,13 @@
  * Time: 9:14
  */
 $file = dirname(__FILE__).'/params.inc';
-$content = file_get_contents($file);
-$arr = unserialize(base64_decode($content));
+$arr=array();
+if(file_exists($file))
+{
+    $content = file_get_contents($file);
+    if(!empty($content))
+        $arr = unserialize(base64_decode($content));
+}
 
 return $arr;
 ?>
