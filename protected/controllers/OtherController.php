96ff6caefa93851c00f9a6361ded339027cf3814
lesha724 <neff.alexey@gmail.com>
2016-04-05 22:13:59 +0300

    1

--- protected/controllers/OtherController.php
+++ protected/controllers/OtherController.php
@@ -1110,7 +1110,7 @@ SQL;
         $id = $uploadResult->UploadDocumentResult->Uploaded[0]->Id;
 
         // Иницировать проверку с использованием собственного модуля поиска и модуля поиска "wikipedia"
-        $client->CheckDocument(array("docId" => $id, "checkServicesList" => array($COMPANY_NAME, "internet", "disser.rsl", "lexpro")));
+        $client->CheckDocument(array("docId" => $id));
 
         // Получить текущий статус последней проверки
         $status = $client->GetCheckStatus(array("docId" => $id));
