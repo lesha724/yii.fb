61d85cdf2170aed52669c1054364b009524f4b7e
lesha724 <neff.alexey@gmail.com>
2016-05-13 21:18:18 +0300

    1

--- protected/migrations/m160313_130122_add_st163.php
+++ protected/migrations/m160313_130122_add_st163.php
@@ -7,7 +7,7 @@ class m160313_130122_add_st163 extends CDbMigration
 		$sql = <<<SQL
 			alter table st add st163 dat;
 SQL;
-		$this->execute($sql);
+		//$this->execute($sql);
 	}
 
 	public function safeDown()
