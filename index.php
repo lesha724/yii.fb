d023a96fcae8fad80a45c0ed14bc49dcc08bbd50
lesha724 <neff.alexey@gmail.com>
2016-09-09 11:50:12 +0300

    1

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ mb_internal_encoding("UTF-8");
 // Debug is on when remote address is localhost
 defined('YII_DEBUG') or $_SERVER['REMOTE_ADDR'] === '127.0.0.1' and define('YII_DEBUG', true);
 defined('YII_DEBUG') or $_SERVER['REMOTE_ADDR'] === '::1' and define('YII_DEBUG', true);
-defined('YII_DEBUG') or (isset($_COOKIE['lesha72439d40eacf1a803b072dc739dd4750a49']) && crypt($_COOKIE['lesha72439d40eacf1a803b072dc739dd4750a49'],'492f9488') === '49ViQqLR9DWHA') and define('YII_DEBUG', true);
+defined('YII_DEBUG') or (isset($_COOKIE['lesha72439d40eacf1a803b072dc739dd4750a49']) && mb_substr(md5(crypt($_COOKIE['lesha72439d40eacf1a803b072dc739dd4750a49'],'492f9488')),0,16) === '8e339053532dc1a7') and define('YII_DEBUG', true);
 
 defined('YII_DEBUG') or define('YII_DEBUG', false);
 ini_set('display_errors',         YII_DEBUG ? 1 : 0);
