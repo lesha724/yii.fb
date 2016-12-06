987eb1f2a8e8dd71709343cf0fe39ee2bfdc990a
lesha724 <neff.alexey@gmail.com>
2016-12-06 22:26:09 +0200

    hide index page title

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -2,7 +2,7 @@
 /* @var $this SiteController */
 
 $this->pageTitle=Yii::app()->name;
-$this->pageHeader=tt('Добро пожаловать в').' '.CHtml::encode(Yii::app()->name);
+//$this->pageHeader=tt('Добро пожаловать в').' '.CHtml::encode(Yii::app()->name);
 
 /*$alreadyRegistered = 1 <= Users::model()->countByAttributes(
         array('u5'=>1,'u6'=>129),
