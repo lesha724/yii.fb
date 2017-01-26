0874f1b6f34287ea8cdbf257e6cf5f55dbf0d6ce
lesha724 <neff.alexey@gmail.com>
2017-01-26 15:56:35 +0200

    1

--- protected/models/ResetPasswordForm.php
+++ protected/models/ResetPasswordForm.php
@@ -42,7 +42,7 @@ class ResetPasswordForm extends CFormModel
 		);
 	}
 
-	public function __construct($token, $config = [])
+	public function __construct($token, $config = array())
 	{
 		if (empty($token) || !is_string($token)) {
 			throw new Exception('Password reset token cannot be blank.');
