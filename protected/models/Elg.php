f99da97f8593f5ed0780e57c4303cf40563ece61
lesha724 <neff.alexey@gmail.com>
2017-02-08 14:05:04 +0200

    1

--- protected/models/Elg.php
+++ protected/models/Elg.php
@@ -238,7 +238,7 @@ SQL;
 					  WHEN 1 THEN 0
 					  ELSE 1
 					END) as type_journal,
-					us6,k2,k15,uo3,u16,u1,d1,d27,d32,d34,d36,uo1,sem1, sem7,ucgn2
+					k2,k15,uo3,u16,u1,d1,d27,d32,d34,d36,uo1,sem1, sem7,ucgn2
                     from ucxg
                        inner join ucgn on (ucxg.ucxg2 = ucgn.ucgn1)
                        inner join ucx on (ucxg.ucxg1 = ucx.ucx1)
@@ -251,7 +251,7 @@ SQL;
                        inner join k on (uo.uo4 = k.k1)
                        inner join sem on (us.us3 = sem.sem1)
                     WHERE ucxg3=0 and ucsn2=:ST1 and sem3=:YEAR and sem5=:SEM and us6<>0 and us4 in (1,2,3,4)
-                    group by d2,type_journal,us6,k2, k15,uo3,u16,u1,d1,d27,d32,d34,d36,uo1,sem1, sem7,ucgn2
+                    group by d2,type_journal,k2, k15,uo3,u16,u1,d1,d27,d32,d34,d36,uo1,sem1, sem7,ucgn2
                     ORDER BY d2,type_journal,uo3
 SQL;
 		$command = Yii::app()->db->createCommand($sql);
