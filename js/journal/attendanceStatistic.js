91f9882fea2acc35d57e7d6840939aad86818d31
lesha724 <neff.alexey@gmail.com>
2017-03-22 20:37:36 +0200

    1

--- js/journal/attendanceStatistic.js
+++ js/journal/attendanceStatistic.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
             if (value.length == 0)
                 continue;
 
-            $select.find('option[value='+$that.val()+']').attr('selected', 'selected');
+            $select.find('option[value="'+$that.val()+'"]').attr('selected', 'selected');
 
             $form.append($select);
         }
