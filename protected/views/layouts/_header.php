49419429be58d68d8ae4cc7babe691f92c0af9bd
Alexey Neff <neff.alexey@gmail.com>
2017-12-13 11:13:18 +0200

    1

--- protected/views/layouts/_header.php
+++ protected/views/layouts/_header.php
@@ -55,7 +55,7 @@
                             <img alt="photo" src="<?=$url?>" class="nav-user-photo">
                             <span class="user-info">
                                 <small>Welcome,</small>
-                                <?=Yii::app()->user->model->name?>
+                                <?=Yii::app()->user->name?>
                             </span>
                         <?php endif; ?>
                         <i class="icon-caret-down"></i>
