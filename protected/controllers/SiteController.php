a5cc476624ce75d46910c9055cff1bba9b106a12
Alexey Neff <neff.alexey@gmail.com>
2017-12-13 14:26:28 +0200

    1

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -24,7 +24,7 @@ class SiteController extends Controller
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
 		if(Yii::app()->user->isPrnt)
-			$this->redirect("other/studentCard");
+			$this->redirect("/other/studentCard");
 		else
 			$this->render('index');
 	}
