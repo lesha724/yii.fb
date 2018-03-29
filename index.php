c7c9ec85b61496d5b8482bb18c3518a4042bc357
Alexey Neff <neff.alexey@gmail.com>
2018-03-29 18:32:52 +0300

    v0.1.3.6

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 mb_internal_encoding("UTF-8");
 
-define('ASU_PORTAL_VERSION', '0.1.3.5');
+define('ASU_PORTAL_VERSION', '0.1.3.6');
 
 // Debug is on when remote address is localhost
 defined('YII_DEBUG') or $_SERVER['REMOTE_ADDR'] === '127.0.0.1' and define('YII_DEBUG', true);
