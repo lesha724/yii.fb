cb81c44a66a07ce19f7d7dd0ec1dfc3e8ee18449
Alexey Neff <neff.alexey@gmail.com>
2018-04-25 11:19:42 +0300

    fix #farm-15, #farm-17, #farm-18

--- protected/controllers/ListController.php
+++ protected/controllers/ListController.php
@@ -101,56 +101,104 @@ class ListController extends Controller
         $objPHPExcel->setActiveSheetIndex(0);
         $sheet=$objPHPExcel->getActiveSheet();
 
-        $year = Yii::app()->session['year'];
-        $sem = Yii::app()->session['sem'];
+        list($year, $sem) = SH::getCurrentYearAndSem();
+
         $discipline = D::model()->findByPk($model->discipline);
+        $disciplineName = $discipline->d2;
+        if(Yii::app()->language=='en' && !(empty($discipline->d27))){
+            $disciplineName = $discipline->d27;
+        }
+
         $group = Gr::model()->getVirtualGroupByDiscipline($model->group);
+        $groupInfo = $group->getInfo();
+        $groupName = $group['gr3'];
 
-        $sheet->setCellValue('A1', tt('Дисциплина').': ');
-        $sheet->setCellValue('B1', $discipline->d2);
+        $speciality = isset($groupInfo['pnsp2']) ? $groupInfo['pnsp2'] : '';
+        $faculty = isset($groupInfo['f3']) ? $groupInfo['f3'] : '';
 
-        $sheet->setCellValue('A2', tt('Год').': ');
-        $sheet->setCellValue('B2', $year.'/'.($year+1));
+        if(Yii::app()->language=='en' && !(empty($groupInfo))){
+            if(!empty($groupInfo['pnsp17']))
+                $speciality = $groupInfo['pnsp17'];
 
-        $sheet->setCellValue('A3', tt('Семестр').': ');
-        $sheet->setCellValue('B3',  SH::convertSem5($sem));
+            if(!empty($groupInfo['f26']))
+                $faculty = $groupInfo['f26'];
+        }
 
-        $sheet->setCellValue('A4', tt('Название группы').': ');
-        $sheet->setCellValue('B4',  $group['gr3']);
+        $sheet->mergeCellsByColumnAndRow(0, 2, 2, 2);
+        $sheet->setCellValue('A2', tt('Список студентов'))->getStyle('A2')->getAlignment()-> setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER)->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
+
+        $sheet->mergeCellsByColumnAndRow(0, 3, 2, 3);
+        $sheet->setCellValue('A3', tt('{course} курса {group} группы {speciality} специальности',array(
+            '{course}' => $model->course,
+            '{group}' => $groupName,
+            '{speciality}' => $speciality
+        )))->getStyle('A3')->getAlignment()-> setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER)->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
+
+        $sheet->mergeCellsByColumnAndRow(0, 4, 2, 4);
+        $sheet->setCellValue('A4', tt('{faculty} факультета', array(
+            '{faculty}' => $faculty
+        )))->getStyle('A4')->getAlignment()-> setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER)->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
 
 
-        $rowStart=6;
+        $sheet->mergeCellsByColumnAndRow(0, 5, 2, 5);
+        $sheet->setCellValue('A5', tt(' за {sem} семестр {year} учебный год', array(
+            '{sem}' => SH::convertSem5($sem),
+            '{year}' => $year.'/'.($year+1)
+        )).',')->getStyle('A5')->getAlignment()-> setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER)->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
+
+        $sheet->mergeCellsByColumnAndRow(0, 6, 2, 6);
+        $sheet->setCellValue('A5', tt('которые изучают дисциплину {discipline}', array(
+                '{discipline}' => $disciplineName
+            )))->getStyle('A5')->getAlignment()-> setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER)->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
+
+
+        $sheet->getStyle('A2:A5')->getFont()->setSize(14);
+
+        $rowStart=8;
 
         $sheet->setCellValue('A'.$rowStart,"№");
-        $sheet->setCellValue('B'.$rowStart, tt('ФИО'));
-        $sheet->setCellValue('C'.$rowStart, tt('Академ. группа'));
-        $sheet->setCellValue('D'.$rowStart,'№ '.tt('зач. книжки'));
+        $sheet->setCellValue('B'.$rowStart, tt('ФИО студента'));
+        $sheet->setCellValue('C'.$rowStart,'№ '.tt('зач. книжки'));
+        $sheet->setCellValue('D'.$rowStart, tt('Академ. группа'));
 
-        $sheet->getColumnDimensionByColumn(0)->setWidth(20);
+
+        $sheet->getStyle('A'.$rowStart.':D'.$rowStart)->getAlignment()-> setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER)->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
+        $sheet->getStyle('A'.$rowStart.':D'.$rowStart)->getFont()->setBold( true );
+
+
+        $sheet->getColumnDimensionByColumn(0)->setWidth(5);
         $sheet->getColumnDimensionByColumn(1)->setWidth(40);
-        $sheet->getColumnDimensionByColumn(2)->setWidth(20);
-        $sheet->getColumnDimensionByColumn(3)->setWidth(20);
+        $sheet->getColumnDimensionByColumn(2)->setWidth(12);
+        $sheet->getColumnDimensionByColumn(3)->setWidth(10);
 
         $i=1;
         foreach($students as $student):
             $name = $student['st2'].' '.$student['st3'].' '.$student['st4'];
+            if(Yii::app()->language == 'en' && !empty($student['st74']))
+                $name = $student['st74'].' '.$student['st75'].' '.$student['st76'];
 
             $sheet->setCellValueByColumnAndRow(0,$i+ $rowStart,$i);
             $sheet->setCellValueByColumnAndRow(1,$i+ $rowStart,$name);
-            $sheet->setCellValueByColumnAndRow(2,$i+ $rowStart,Gr::model()->getGroupName($model->course, $student));
-            $sheet->setCellValueByColumnAndRow(3,$i+ $rowStart,$student['st5']);
+            $sheet->setCellValueByColumnAndRow(2,$i+ $rowStart,$student['st5']);
+            $sheet->setCellValueByColumnAndRow(3,$i+ $rowStart,Gr::model()->getGroupName($model->course, $student));
+
             $i++;
         endforeach;
 
+        $sheet->getStyle('A'.$rowStart.':A'.($rowStart+$i-1))->getAlignment()-> setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER)->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
+        $sheet->getStyle('C'.$rowStart.':C'.($rowStart+$i-1))->getAlignment()-> setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER)->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
 
-        $sheet->setTitle('ListVirtualGroup');
+
+        $sheet->getStyleByColumnAndRow(0,$rowStart,3,$i+$rowStart-1)->getBorders()->getAllBorders()->applyFromArray(array('style'=>PHPExcel_Style_Border::BORDER_THIN,'color' => array('rgb' => '000000')));
+
+        $sheet->setTitle(tt('Список виртуальной группы '. $groupName. '/дисциплина '.$disciplineName));
 
         // Set active sheet index to the first sheet, so Excel opens this as the first sheet
         $objPHPExcel->setActiveSheetIndex(0);
 
         // Redirect output to a clientâ€™s web browser (Excel5)
         header('Content-Type: application/vnd.ms-excel');
-        header('Content-Disposition: attachment;filename="ACY_ListVirtualGroup_'.date('Y-m-d H-i').'.xls"');
+        header('Content-Disposition: attachment;filename="ACY_ListVirtualGroup_'.$groupName.'_'.date('Y-m-d H-i').'.xls"');
         header('Cache-Control: max-age=0');
         // If you're serving to IE 9, then the following may be needed
         header('Cache-Control: max-age=1');
@@ -270,7 +318,7 @@ class ListController extends Controller
 
         $sheet->setTitle(tt('Список группы'). ' '. $groupName);
 
-        $sheet->getStyleByColumnAndRow(0,7,2,$i+$rowStart-1)->getBorders()->getAllBorders()->applyFromArray(array('style'=>PHPExcel_Style_Border::BORDER_THIN,'color' => array('rgb' => '000000')));
+        $sheet->getStyleByColumnAndRow(0,$rowStart,2,$i+$rowStart-1)->getBorders()->getAllBorders()->applyFromArray(array('style'=>PHPExcel_Style_Border::BORDER_THIN,'color' => array('rgb' => '000000')));
 
 
         // Set active sheet index to the first sheet, so Excel opens this as the first sheet
