6125a74f82f503922a46878adf3e4c9629083afb
Alexey Neff <neff.alexey@gmail.com>
2018-05-09 00:19:50 +0300

    add for new attendance statisstic

--- protected/views/journal/newAttendanceStatistic/_bottom_other.php
+++ protected/views/journal/newAttendanceStatistic/_bottom_other.php
@@ -10,3 +10,18 @@
 /** @var $this JournalController */
 
 $students = $model->getStudents();
+list($firstDay, $lastDay) = Sem::model()->getSemesterStartAndEnd($model->semester);
+
+$th = $tr = '';
+//заголовок для таблицы с пз
+$th.='<tr>';
+$th.='<th>'.tt('№ пп').'</th>';
+$th.='<th>'.tt('Количество прошедших занятий').'</th>';
+$th.='<th>'.tt('Количество пропусков').'</th>';
+$th.='<th>'.tt('Количество уваж. пропусков').'</th>';
+$th.='</tr>';
+
+foreach ($students as $student){
+
+    list($respectful,$disrespectful,$count) = Elg::model()->getAttendanceStatisticInfoByDate($firstDay, $lastDay, $student['st1']);
+}
