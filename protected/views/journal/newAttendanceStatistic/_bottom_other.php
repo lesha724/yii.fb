51cb371c37b878df0f71ae236beea17d96eaaed4
Alexey Neff <neff.alexey@gmail.com>
2018-05-10 16:46:45 +0300

    fix new statistic

--- protected/views/journal/newAttendanceStatistic/_bottom_other.php
+++ protected/views/journal/newAttendanceStatistic/_bottom_other.php
@@ -12,16 +12,47 @@
 $students = $model->getStudents();
 list($firstDay, $lastDay) = Sem::model()->getSemesterStartAndEnd($model->semester);
 
+$table = <<<HTML
+    <table id="newAttendanceStatistic-table"  class="table table-bordered table-hover table-condensed">
+        <thead>
+                %s
+        </thead>
+        <tbody>
+            %s
+        </tbody>
+    </table>
+HTML;
+
 $th = $tr = '';
 //заголовок для таблицы с пз
 $th.='<tr>';
 $th.='<th>'.tt('№ пп').'</th>';
+$th.='<th>'.tt('Студент').'</th>';
+$th.='<th>'.tt('Группа').'</th>';
 $th.='<th>'.tt('Количество прошедших занятий').'</th>';
-$th.='<th>'.tt('Количество пропусков').'</th>';
 $th.='<th>'.tt('Количество уваж. пропусков').'</th>';
+$th.='<th>'.tt('Количество неуваж. пропусков').'</th>';
 $th.='</tr>';
+
+$i = 1;
 
 foreach ($students as $student){
 
+    $name = $student['st2'].' '.$student['st3'].' '.$student['st4'];
+    $group = Gr::model()->getGroupName($model->course, $student);
     list($respectful,$disrespectful,$count) = Elg::model()->getAttendanceStatisticInfoByDate($firstDay, $lastDay, $student['st1']);
+    $tr.=<<<HTML
+        <tr>
+            <td>{$i}</td>
+            <td>{$name}</td>
+            <td>{$group}</td>
+            <td>{$count}</td>
+            <td>{$respectful}</td>
+            <td>{$disrespectful}</td>
+        </tr>
+HTML;
+
+    $i++;
 }
+
+echo sprintf($table,$th,$tr);
