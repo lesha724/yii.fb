0f9e14027e2932149286885630d8b64931fc3ee0
Alexey Neff <neff.alexey@gmail.com>
2018-05-15 15:18:08 +0300

    fix show users photo

--- protected/models/Users.php
+++ protected/models/Users.php
@@ -267,7 +267,10 @@ SQL;
         $string = Yii::app()->db->connectionString;
         $parts  = explode('=', $string);
 
-        $host     = trim($parts[1].'d');
+        $host = str_replace(';role', '', $parts[1]);
+
+        $host     = trim($host.'d');
+
         $login    = Yii::app()->db->username;
         $password = Yii::app()->db->password;
         $dbh      = ibase_connect($host, $login, $password);
