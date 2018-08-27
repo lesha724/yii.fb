b65ebdd6407db45f32e79abefdb57ed63f482afd
Alexey Neff <neff.alexey@gmail.com>
2018-08-27 20:52:21 +0300

    1

--- protected/migrations/m160915_100823_alter_users_history.php
+++ protected/migrations/m160915_100823_alter_users_history.php
@@ -4,16 +4,19 @@ class m160915_100823_alter_users_history extends CDbMigration
 {
 	public function safeUp()
 	{
-		$sql = 'update RDB$RELATION_FIELDS set
+		/*$sql = 'update RDB$RELATION_FIELDS set
 			RDB$FIELD_SOURCE = \'VAR200\'
 			where (RDB$FIELD_NAME = \'UH4\') and
-			(RDB$RELATION_NAME = \'USERS_HISTORY\')';
+			(RDB$RELATION_NAME = \'USERS_HISTORY\')';*/
+
+		$sql = 'ALTER TABLE USERS_HISTORY ALTER COLUMN UH4 TYPE VAR200';
 		$this->execute($sql);
 
-		$sql = 'update RDB$RELATION_FIELDS set
+		/*$sql = 'update RDB$RELATION_FIELDS set
 			RDB$FIELD_SOURCE = \'VAR200\'
 			where (RDB$FIELD_NAME = \'UH6\') and
-			(RDB$RELATION_NAME = \'USERS_HISTORY\')';
+			(RDB$RELATION_NAME = \'USERS_HISTORY\')';*/
+        $sql = 'ALTER TABLE USERS_HISTORY ALTER COLUMN UH6 TYPE VAR200';
 		$this->execute($sql);
 	}
 
