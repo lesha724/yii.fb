22f65d54a9822935bc182f9d1d0ab3a4eaa5f94e
Alexey Neff <neff.alexey@gmail.com>
2019-02-04 17:37:18 +0200

    1

--- protected/views/portfolio/student/_bottom.php
+++ protected/views/portfolio/student/_bottom.php
@@ -10,17 +10,25 @@
  * @var St $student
  */
 
+echo '<div class="page-header">';
 echo CHtml::tag('h3', array(), tt('Дисциплины'));
+echo '</div>';
 $this->renderPartial('student/_table1', array(
     'student' => $student
 ));
 
+
+echo '<div class="page-header">';
 echo CHtml::tag('h3', array(), tt('Документы подтверждающие в научно-иследовательской деятельности'));
+echo '</div>';
 $this->renderPartial('student/_table2', array(
     'student' => $student
 ));
 
+
+echo '<div class="page-header">';
 echo CHtml::tag('h3', array(), tt('Документы подтверждающие участие в общественной деятельности'));
+echo '</div>';
 $this->renderPartial('student/_table3', array(
     'student' => $student
 ));
