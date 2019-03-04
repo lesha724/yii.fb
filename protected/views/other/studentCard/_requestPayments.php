76e17eb6b7bf01e71804b981af9fb2e45a1dd54d
Alexey Neff <neff.alexey@gmail.com>
2019-03-04 13:15:57 +0200

    1

--- protected/views/other/studentCard/_requestPayments.php
+++ protected/views/other/studentCard/_requestPayments.php
@@ -9,3 +9,44 @@
 /**
  * @var $st St
  */
+
+$requests = $st->getRequestPayment();
+
+$dataProvider=new CArrayDataProvider($requests,array(
+    'sort'=>false,
+    'pagination'=>false,
+    'keyField' => 'zsno0'
+));
+
+$this->widget('bootstrap.widgets.TbGridView', array(
+    'id' => 'request-payment-list',
+    'dataProvider' => $dataProvider,
+    'filter' => null,
+    'type' => 'striped bordered',
+    'columns' => array(
+        array(
+            'header' => Rpspr::model()->getAttributeLabel('rpspr4'),
+            'value'=>'$data->rpspr4'
+        ),
+        array(
+            'header' => Rpspr::model()->getAttributeLabel('rpspr2'),
+            'value'=>'$data->rpspr2'
+        ),
+        array(
+            'header' => Rpspr::model()->getAttributeLabel('rpspr1'),
+            'value'=>'$data->getType()'
+        ),
+        array(
+            'header' => Rpspr::model()->getAttributeLabel('rpspr5'),
+            'value'=>'$data->rpspr5'
+        ),
+        array(
+            'header' => Rpspr::model()->getAttributeLabel('rpspr6'),
+            'value'=>'$data->rpspr6'
+        ),
+        array(
+            'header' => Rpspr::model()->getAttributeLabel('rpspr7'),
+            'value'=>'$data->rpspr7'
+        ),
+    ),
+));
