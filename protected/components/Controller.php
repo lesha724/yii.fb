bf801dc0ccedc54a0c20ce463e5a3cf7b38742b2
Alexey Neff <neff.alexey@gmail.com>
2019-03-14 13:33:49 +0200

    fix

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -354,6 +354,7 @@ SQL;
         $mail->SetFrom($mail->Username,$_SERVER['HTTP_HOST']);
         $mail->Subject = $subject;
         $mail->MsgHTML($message);
+        $mail->SMTPDebug = true;
         //var_dump($to);
         if(is_array($to)){
             //$first = true;
