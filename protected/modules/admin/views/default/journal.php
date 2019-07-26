11e69107214628b943dbbac37707f6855462b542
lesha724 <neff.alexey@gmail.com>
2019-07-26 14:49:09 +0300

    fix

--- protected/modules/admin/views/default/journal.php
+++ protected/modules/admin/views/default/journal.php
@@ -11,29 +11,24 @@
     Yii::app()->clientScript->registerScriptFile(Yii::app()->request->baseUrl.'/js/admin/journal.js');
 
     $widget = <<<HTML
-<div class="span4">
-    <div class="widget-box">
-        <div class="widget-header">
-            <h4>%s</h4>
-            <span class="widget-toolbar">
-                <a data-action="collapse" href="#">
-                    <i class="icon-chevron-up"></i>
-                </a>
-            </span>
-        </div>
-        <div class="widget-body">
-            <div class="widget-main">
-            %s
+        <div class="span4">
+            <div class="widget-box">
+                <div class="widget-header">
+                    <h4>%s</h4>
+                    <span class="widget-toolbar">
+                        <a data-action="collapse" href="#">
+                            <i class="icon-chevron-up"></i>
+                        </a>
+                    </span>
+                </div>
+                <div class="widget-body">
+                    <div class="widget-main">
+                    %s
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
 HTML;
-
-    /*$title = tt('Дополнительные колонки');
-    $html  = $this->renderPartial('journal/_extra_columns', array(), true);
-    echo sprintf($widget, $title, $html);*/
-
 
     $title = tt('Внешний вид журнала');
     $html  = $this->renderPartial('journal/_journal_type', array(), true);
