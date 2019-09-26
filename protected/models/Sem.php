24e0c6132c36bf52ea8aba2403f78fbb67f0ed84
lesha724 <neff.alexey@gmail.com>
2019-09-26 18:16:22 +0300

    fix

--- protected/models/Sem.php
+++ protected/models/Sem.php
@@ -45,83 +45,7 @@ class Sem extends CActiveRecord
 			array('sem1, sem2, sem3, sem4, sem5, sem7, sem9, sem19, sem20, sem21, sem22', 'numerical', 'integerOnly'=>true),
 			array('sem10, sem11, sem12, sem13, sem14, sem15, sem18', 'length', 'max'=>8),
 			array('sem17', 'length', 'max'=>4),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('sem1, sem2, sem3, sem4, sem5, sem7, sem9, sem10, sem11, sem12, sem13, sem14, sem15, sem17, sem18, sem19, sem20, sem21, sem22', 'safe', 'on'=>'search'),
 		);
-	}
-
-
-
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'sem1' => 'Sem1',
-			'sem2' => 'Sem2',
-			'sem3' => 'Sem3',
-			'sem4' => 'Sem4',
-			'sem5' => 'Sem5',
-			'sem7' => 'Sem7',
-			'sem9' => 'Sem9',
-			'sem10' => 'Sem10',
-			'sem11' => 'Sem11',
-			'sem12' => 'Sem12',
-			'sem13' => 'Sem13',
-			'sem14' => 'Sem14',
-			'sem15' => 'Sem15',
-			'sem17' => 'Sem17',
-			'sem18' => 'Sem18',
-			'sem19' => 'Sem19',
-			'sem20' => 'Sem20',
-			'sem21' => 'Sem21',
-			'sem22' => 'Sem22',
-		);
-	}
-
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 *
-	 * Typical usecase:
-	 * - Initialize the model fields with values from filter form.
-	 * - Execute this method to get CActiveDataProvider instance which will filter
-	 * models according to data in model fields.
-	 * - Pass data provider to CGridView, CListView or any similar widget.
-	 *
-	 * @return CActiveDataProvider the data provider that can return the models
-	 * based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// @todo Please modify the following code to remove attributes that should not be searched.
-
-		$criteria=new CDbCriteria;
-
-		$criteria->compare('sem1',$this->sem1);
-		$criteria->compare('sem2',$this->sem2);
-		$criteria->compare('sem3',$this->sem3);
-		$criteria->compare('sem4',$this->sem4);
-		$criteria->compare('sem5',$this->sem5);
-		$criteria->compare('sem7',$this->sem7);
-		$criteria->compare('sem9',$this->sem9);
-		$criteria->compare('sem10',$this->sem10,true);
-		$criteria->compare('sem11',$this->sem11,true);
-		$criteria->compare('sem12',$this->sem12,true);
-		$criteria->compare('sem13',$this->sem13,true);
-		$criteria->compare('sem14',$this->sem14,true);
-		$criteria->compare('sem15',$this->sem15,true);
-		$criteria->compare('sem17',$this->sem17,true);
-		$criteria->compare('sem18',$this->sem18,true);
-		$criteria->compare('sem19',$this->sem19);
-		$criteria->compare('sem20',$this->sem20);
-		$criteria->compare('sem21',$this->sem21);
-		$criteria->compare('sem22',$this->sem22);
-
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
 	}
 
 	/**
@@ -134,7 +58,12 @@ class Sem extends CActiveRecord
 	{
 		return parent::model($className);
 	}
-	
+
+    /**
+     * @param $gr1
+     * @return array
+     * @throws CException
+     */
 	public function getSemestrForGroup($gr1)
     {
 		 if (empty($gr1))
@@ -144,16 +73,11 @@ class Sem extends CActiveRecord
 			   inner join gr on (sg.sg1 = gr.gr2)
 			where gr.gr1='.$gr1.' and sem.sem10<=\''.date('d.m.Y',strtotime("+15 day")).'\' and sem.sem11>=\''.date('d.m.Y',strtotime("+15 day")).'\'';
 		$command = Yii::app()->db->createCommand($sql);
-		//print_r($sql);
-		/*$command->bindValue(':gr1', $gr1);
-		$command->bindValue(':nac', date('d.m.Y'));*/
+
 		$sem = $command->queryRow();
 		
 		$date1 = $sem['sem10'];
         $date2 = $sem['sem11'];
-
-        /*if (empty($date1) || empty($date2))
-            Yii::app()->user->setFlash('error', tt('Неуказаны дата начала и/или дата окончания семестра'));*/
 
         return array($date1, $date2);
 	}
@@ -173,64 +97,8 @@ class Sem extends CActiveRecord
 
         return !empty($sem1)?$sem1:0;
     }
-	
-    public function getYearsForThematicPlan($speciality)
-    {
-        if (empty($speciality))
-            return array(array(), array());
 
-        $sql=<<<SQL
-            SELECT sg3,sg4,sg1,sem4
-            FROM sem
-            INNER JOIN sg on (sem.sem2 = sg.sg1)
-            WHERE sg2=:SPECIALITY and (sem3=:YEAR1 or sem3=:YEAR2)
-            GROUP BY sg3,sg4,sg1,sem4
-            ORDER BY sg4,sg3 DESC,sem4 DESC
-SQL;
-
-        $command = Yii::app()->db->createCommand($sql);
-        $command->bindValue(':SPECIALITY', $speciality);
-        $command->bindValue(':YEAR1', date('Y', strtotime('previous year')));
-        $command->bindValue(':YEAR2', date('Y'));
-        $years = $command->queryAll();
-
-        foreach ($years as $key => $year) {
-            $years[$key]['name'] = $year['sg3'].' ('.SH::convertEducationType($year['sg4']).') '.$year['sem4'].' '.tt('курс');
-        }
-
-        $dataAttrs = array();
-        foreach ($years as $year) {
-            $dataAttrs[$year['sg1']] = array('data-sem4' => $year['sem4']);
-        }
-
-        return array($years, $dataAttrs);
-    }
-
-    public function getSemestersForThematicPlan1($uo1)
-    {
-        if (empty($uo1))
-            return array();
-
-        $sql=<<<SQL
-           SELECT us4,sem7,sem3,sem5,sem1,us1
-           FROM sem
-           INNER JOIN us on (sem.sem1 = us.us3)
-           INNER JOIN uo on (us.us2 = uo.uo1)
-           WHERE us2=:UO1 and us4 in (1,2,3,4)
-           GROUP BY us4,sem7,sem3,sem5,sem1,us1
-SQL;
-
-        $command = Yii::app()->db->createCommand($sql);
-        $command->bindValue(':UO1', $uo1);
-        $semesters = $command->queryAll();
-
-        foreach ($semesters as $key => $sem) {
-            $semesters[$key]['name'] = SH::convertUS4($sem['us4']).', '.$sem['sem7'].' '.tt('сем').'. ('.$sem['sem3'].', '.SH::convertSem5($sem['sem5']).')';
-        }
-
-        return $semesters;
-    }
-
+    ///TODO: Обьеденить с getSemestersForRating
     public function getSemestersForWorkPlan($gr1, $type)
     {
         if ($type == WorkPlanController::SPECIALITY)
@@ -264,30 +132,8 @@ SQL;
 
         return $semesters;
     }
-	
-	public function getSemestersForThematicPlan($sg1)
-    {
-            $sql = <<<SQL
-                SELECT sem3,sem4,sem5,sem7,us3
-                FROM us
-                INNER JOIN sem on (us.us3 = sem.sem1)
-                INNER JOIN sg on (sem.sem2 = sg.sg1)
-                INNER JOIN gr on (sg.sg1 = gr.gr2)
-                WHERE sg1=:ID
-                GROUP BY sem3,sem4,sem5,sem7,us3
-SQL;
 
-        $command = Yii::app()->db->createCommand($sql);
-        $command->bindValue(':ID', $sg1);
-        $semesters = $command->queryAll();
-
-        foreach ($semesters as $key => $sem) {
-            $semesters[$key]['name'] = $sem['sem3'].' ('.$sem['sem4'].' '.tt('курс').')';
-        }
-
-        return $semesters;
-    }
-	
+    ///TODO: Обьеденить с getSemestersForWorkPlan
 	public function getSemestersForRating($gr1, $type)
     {
         if ($type == RatingForm::SPECIALITY)
@@ -316,7 +162,6 @@ SQL;
         $semesters = $command->queryAll();
 
         foreach ($semesters as $key => $sem) {
-            //$semesters[$key]['name'] = tt('Семестр').' №'.$sem['sem7'].' ('.$sem['sem3'].' '.$sem['sem4'].' '.tt('курс').')';
             $semesters[$key]['name'] = $sem['sem3'].' ('.$sem['sem4'].' '.tt('курс').')';
         }
 
--- protected/models/Stusvst.php
+++ protected/models/Stusvst.php
@@ -33,59 +33,7 @@ class Stusvst extends CActiveRecord
 			array('stusvst1, stusvst3, stusvst4, stusvst6, stusvst8', 'numerical', 'integerOnly'=>true),
 			array('stusvst5, stusvst7', 'length', 'max'=>30),
             array('stusvst5', 'default', 'value'=>'', 'setOnEmpty'=>TRUE),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('stusvst1, stusvst3, stusvst4, stusvst5, stusvst6, stusvst7, stusvst8', 'safe', 'on'=>'search'),
 		);
-	}
-
-
-
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'stusvst1' => 'Stusvst1',
-			'stusvst3' => 'Stusvst3',
-			'stusvst4' => 'Stusvst4',
-			'stusvst5' => 'Stusvst5',
-			'stusvst6' => 'Stusvst6',
-			'stusvst7' => 'Stusvst7',
-			'stusvst8' => 'Stusvst8',
-		);
-	}
-
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 *
-	 * Typical usecase:
-	 * - Initialize the model fields with values from filter form.
-	 * - Execute this method to get CActiveDataProvider instance which will filter
-	 * models according to data in model fields.
-	 * - Pass data provider to CGridView, CListView or any similar widget.
-	 *
-	 * @return CActiveDataProvider the data provider that can return the models
-	 * based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// @todo Please modify the following code to remove attributes that should not be searched.
-
-		$criteria=new CDbCriteria;
-
-		$criteria->compare('stusvst1',$this->stusvst1);
-		$criteria->compare('stusvst3',$this->stusvst3);
-		$criteria->compare('stusvst4',$this->stusvst4);
-		$criteria->compare('stusvst5',$this->stusvst5,true);
-		$criteria->compare('stusvst6',$this->stusvst6);
-		$criteria->compare('stusvst7',$this->stusvst7,true);
-		$criteria->compare('stusvst8',$this->stusvst8);
-
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
 	}
 
 	/**
