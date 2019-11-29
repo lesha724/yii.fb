8f24bdcdc540220f4892c956b6dbb5fd3adda5f3
lesha724 <neff.alexey@gmail.com>
2019-11-29 11:18:02 +0200

    1

--- protected/config/messages.php
+++ protected/config/messages.php
@@ -6,7 +6,7 @@
 return array(
     'sourcePath' => dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR,
     'messagePath'=> dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'messages',
-    'languages'  => array('en','uk','ru'),
+    'languages'  => array('en','uk'),
     'fileTypes'  => array('php'),
     'overwrite'  => false,
     'translator' => 'tt'
