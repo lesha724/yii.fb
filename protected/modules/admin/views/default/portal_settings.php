4ca97779451cc8771cb326b867ca9d5e65c689e7
lesha724 <neff.alexey@gmail.com>
2020-02-13 12:11:30 +0200

    v0.3.25

--- protected/modules/admin/views/default/portal_settings.php
+++ protected/modules/admin/views/default/portal_settings.php
@@ -67,12 +67,6 @@ Yii::app()->clientScript->registerScript('sem-start', $js);
                 </div>
 
                 <div class="control-group">
-                    <?=CHtml::checkBox('', PortalSettings::model()->findByPk(107)->ps2, $checkboxStyle)?>
-                    <span class="lbl"> <?=tt('Использовать стусв')?></span>
-                    <?=CHtml::hiddenField('settings[107]', PortalSettings::model()->findByPk(107)->ps2)?>
-                </div>
-
-                <div class="control-group">
                     <?=CHtml::checkBox('', PortalSettings::model()->findByPk(80)->ps2, $checkboxStyle)?>
                     <span class="lbl"> <?=tt('Скрывать баннер "Плаймаркет"')?></span>
                     <?=CHtml::hiddenField('settings[80]', PortalSettings::model()->findByPk(80)->ps2)?>
